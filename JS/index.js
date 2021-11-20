ba3136842e80aee8d54e1de2df5a6d9659a9a11d
Michael Buzzard <m.buzzard87@gmail.com>
2021-11-19 21:40:21 -0500

    starting freshgit add .!

--- JS/index.js
+++
@@ -1,8 +0,0 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-
-const container = document.querySelector(".container");
-
-hamburgerMenu.addEventListener("click", () => {
-    container.classList.toggle("active");
-})
-
